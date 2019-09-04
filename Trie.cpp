a8d9bfb1fc3e17fb98418cae855f6229ff12887a
LEEJONGHWA <unknown9732@naver.com>
2019-09-04 16:08:09 +0900

    주석

--- Trie.cpp
+++ Trie.cpp
@@ -4,8 +4,8 @@ using namespace std;
 class Node {
 public:
 	char c;
-	int use, finish;
-	Node *child[26];
+	int use, finish; // 해당 노드 사용표시(초기화할 때 요긴하게 쓰임), 해당 알파벳이 단어의 끝인지 표시
+	Node *child[26]; // 알파벳 소문자는 26개
 };
 
 Node heap[10000];
@@ -16,6 +16,7 @@ void add(Node *node, char *str, int idx) {
 		node->finish = 1;
 		return;
 	}
+	// 'a'는 아스키 코드값이 97 이므로..
 	node->child[str[idx] - 97] = &heap[heapidx++];
 	node->child[str[idx] - 97]->use = 1;
 	node->child[str[idx] - 97]->c = str[idx];
@@ -23,7 +24,7 @@ void add(Node *node, char *str, int idx) {
 }
 
 bool find(Node *node, char *str, int idx) {
-	while (str[idx] != '\0') {
+	while (str[idx] != '\0') { //입력받은 스트링의 끝까지 탐색
 		if (node->child[str[idx] - 97] == nullptr) return false;
 		node = node->child[str[idx] - 97];
 		idx++;

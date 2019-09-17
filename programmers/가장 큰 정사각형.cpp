81de686d981fa10472223d0461af3709698f42b1
LEEJONGHWA <unknown9732@naver.com>
2019-09-17 15:25:37 +0900

    Update 가장 큰 정사각형.cpp

--- programmers/가장 큰 정사각형.cpp
+++ programmers/가장 큰 정사각형.cpp
@@ -11,9 +11,9 @@ int solution(vector<vector<int>> board){
 	int ans = 0;
 	for (int i = 0; i < board.size(); i++) {
 		for (int j = 0; j < board[0].size(); j++) {
-			if (i == 0 || j == 0 || board[i][j] == 0) D[i][j] = board[i][j];
+			if (i == 0 || j == 0 || board[i][j] == 0) D[i][j] = board[i][j]; // 가장자리는 0행 또는 0열은 무조건 0 아니면 1
 			else D[i][j] = 1 + min(D[i - 1][j], min(D[i][j - 1], D[i - 1][j - 1]));
-            ans = max(ans, D[i][j]);
+            	ans = max(ans, D[i][j]);
 		}
 	}
   	return ans * ans;

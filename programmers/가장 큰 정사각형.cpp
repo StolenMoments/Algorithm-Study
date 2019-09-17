ff86874522c866cb589de875ec9639d4c4bf4472
LEEJONGHWA <unknown9732@naver.com>
2019-09-17 15:21:51 +0900

    Create 가장 큰 정사각형.cpp

---
+++ programmers/가장 큰 정사각형.cpp
@@ -0,0 +1,20 @@
+// DP
+// 왼쪽, 위쪽, 대각쪽 칸의 정사각형의 길이를 확인해서 현재 칸의 정사각형 길이를 정하는 방식
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int D[1002][1002];
+int solution(vector<vector<int>> board){
+	int sz = min(board.size(), board[0].size());
+	int ans = 0;
+	for (int i = 0; i < board.size(); i++) {
+		for (int j = 0; j < board[0].size(); j++) {
+			if (i == 0 || j == 0 || board[i][j] == 0) D[i][j] = board[i][j];
+			else D[i][j] = 1 + min(D[i - 1][j], min(D[i][j - 1], D[i - 1][j - 1]));
+            ans = max(ans, D[i][j]);
+		}
+	}
+  return ans * ans;
+}

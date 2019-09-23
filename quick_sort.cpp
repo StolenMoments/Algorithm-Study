71300b81416921f2db43a6f6e401e37ce80f47e3
LEEJONGHWA <unknown9732@naver.com>
2019-09-23 20:29:11 +0900

    Update quick_sort.cpp

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -8,7 +8,7 @@ void quick(int* arr, int l, int r) {
 
 		for (int j = l; j < r; j++)
 		if (arr[j] <= pivot) 	swap(arr[++i], arr[j]); // 피봇보다 작은 값들을 왼쪽으로 몰아 넣는다.
-		swap(arr[i + 1], arr[r]);
+		swap(arr[i + 1], arr[r]); // 피봇과 최초로 만난 피봇보다 큰 값 바꾸기.
 
 		// 나머지 부분 정렬
 		quick(arr, l, i);

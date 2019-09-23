886f3e2afadc1498d9f10e35124b372a1ac36378
LEEJONGHWA <unknown9732@naver.com>
2019-09-23 20:12:15 +0900

    Create quick_sort.cpp

---
+++ quick_sort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void quick(int* arr, int l, int r) {
+	if (l < r) {
+		int pivot = arr[r]; // 끝을 피봇으로 설정
+		int i = l - 1;
+
+		for (int j = l; j < r; j++)
+			if (arr[j] <= pivot) 	swap(arr[++i], arr[j]);
+
+		swap(arr[i + 1], arr[r]);
+    
+    // 나머지 부분 정렬
+		quick(arr, l, i);
+		quick(arr, i + 2, r);
+	}
+
+}
+
+int main() {
+	int arr[10] = { 3,4,1,7,4,5,10,7,9,1 };
+	quick(arr, 0, 9);
+	for (int i = 0; i < 10; i++) cout << arr[i] << ' '; // 1,1,3,4,4,5,6,7,7,9,10
+}

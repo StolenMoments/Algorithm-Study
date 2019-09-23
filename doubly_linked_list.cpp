9d627e849e6c971695417b9061efa851207cf3f6
LEEJONGHWA <unknown9732@naver.com>
2019-09-23 13:15:02 +0900

    클래스 템플릿 적용

--- doubly_linked_list.cpp
+++ doubly_linked_list.cpp
@@ -2,89 +2,98 @@
 #include <queue>
 using namespace std;
 
-class node {
+template<typename T>
+class NODE {
 public:
-	int value;
-	node *prev, *next;
+	T x;
+	NODE* prev, * next;
 };
 
-int heap_idx;
-node heap[200000]; // 힙 영역에서 할당하는 방식이 동적 할당보다 빠르다. 삼성 B형에서 주로 활용하는 방식.
+NODE<int> heap[500000];
+int HEAP_IDX;
 
+template<typename T>
 class LIST {
-public:
-	node *head, *tail;
+private:
+	NODE<T>* head, * tail;
+	int _size = 0;
 
+public:
 	LIST() {
-		head = &heap[heap_idx++];
-		tail = &heap[heap_idx++];
+		head = &heap[HEAP_IDX++];
+		tail = &heap[HEAP_IDX++];
 		head->next = tail;
 		tail->prev = head;
-	}
-
-	void push_front(int value) {
-		node *new_node = &heap[heap_idx++];
-		new_node->value = value;
-
-		head->next->prev = new_node;
-		new_node->prev = head;
-		new_node->next = head->next;
-		head->next = new_node;
-	}
-
-	void push_back(int value) {
-		node *new_node = &heap[heap_idx++];
-		new_node->value = value;
-
-		tail->prev->next = new_node;
-		new_node->next = tail;
-		new_node->prev = tail->prev;
-		tail->prev = new_node;
-	}
-
-	void pop_front() {
-		if (empty()) return;
-
-		head->next->next->prev = head;
-		head->next = head->next->next;
-	}
-
-	void pop_back() {
-		if (empty()) return;
-
-		tail->prev->prev->next = tail;
-		tail->prev = tail->prev->prev;
-	}
-
-	int front() {
-		if (empty()) throw out_of_range("리스트가 비었습니다\n");
-
-		return head->next->value;
-	}
-
-	int back() {
-		if (empty()) throw out_of_range("리스트가 비었습니다\n");
-
-		return tail->prev->value;
 	}
 
 	bool empty() {
 		return head->next == tail;
 	}
 
-	void print() {
-		node *cur = head->next;
-		while (cur != tail) {
-			printf("%d ", cur->value);
-			cur = cur->next;
+	int size() {
+		return _size;
+	}
+
+	void push_front(T x) {
+		_size++;
+		NODE<T>* node = &heap[HEAP_IDX++];
+		node->x = x;
+		head->next->prev = node;
+		node->next = head->next;
+		node->prev = head;
+		head->next = node;
+	}
+
+	void push_back(T x) {
+		_size++;
+		NODE<T>* node = &heap[HEAP_IDX++];
+		node->x = x;
+		tail->prev->next = node;
+		node->next = tail;
+		node->prev = tail->prev;
+		tail->prev = node;
+	}
+
+	void pop_front() {
+		if (_size > 0) {
+			_size--;
+			head->next->next->prev = head;
+			head->next = head->next->next;
 		}
-		printf("\n");
+		else throw out_of_range("list is empty\n");
+	}
+
+	void pop_back() {
+		if (_size > 0) {
+			_size--;
+			tail->prev->prev->next = tail;
+			tail->prev = tail->prev->prev;
+		}
+		else throw out_of_range("list is empty\n");
+	}
+
+	T front() {
+		if (_size == 0) throw out_of_range("list is empty\n");
+		return head->next->x;
+	}
+
+	T back() {
+		if (_size == 0) throw out_of_range("list is empty\n");
+		return tail->prev->x;
+	}
+
+	void print() {
+		NODE<T>* ptr = head->next;
+		while (ptr != tail) {
+			cout << ptr->x << ' ';
+			ptr = ptr->next;
+		}
+		cout << endl;
 	}
 };
 
-
 int main() {
-	LIST list;
+	LIST<int> list;
 	list.push_back(3);
 	list.push_back(4);
 	list.push_back(9);
@@ -109,7 +118,7 @@ int main() {
 		printf("%d %d\n", list.front(), list.back()); // 리스트가 비었으므로 throw error
 	}
 	catch (out_of_range e) {
-		printf("%s", e.what()); // 에러 메시지 출력
+		printf("%s\n", e.what()); // 에러 메시지 출력
 	}
 
 	printf("%d\n", list.empty()); // 1 출력

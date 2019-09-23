39e9c4fb3b8a8fe7269bf502f44086a50b5d1bfe
LEEJONGHWA <unknown9732@naver.com>
2019-09-23 18:15:00 +0900

    Create priority_queue.cpp

---
+++ priority_queue.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_HEAP 1000
+
+template<typename T>
+class heap {
+private:
+	T _heap[MAX_HEAP];
+	int _size = 0;
+
+public:
+	void push(T x) {
+		if (_size == MAX_HEAP) {
+			cout << "최대 크기 도달\n";
+			return;
+		}
+		_heap[_size] = x;
+		int now = _size++;
+		while (now != 0 && _heap[(now - 1) / 2] < _heap[now]) {
+			swap(_heap[now / 2], _heap[now]);
+			now = (now - 1) / 2;
+		}
+	}
+
+	T pop() {
+		if (_size == 0) {
+			cout << "비어있음\n";
+			return 0;
+		}
+
+		_size--;
+		int now, ret = _heap[0];
+
+		if (_heap[1] > _heap[2]) {
+			_heap[0] = _heap[1];
+			_heap[1] = 0;
+			now = 1;
+		}
+		else {
+			_heap[0] = _heap[2];
+			_heap[2] = 0;
+			now = 2;
+		}
+
+		while (now < _size) {
+			if (_heap[now * 2 + 1] < _heap[now * 2 + 2]) {
+				swap(_heap[now * 2 + 2], _heap[now]);
+				now = now * 2 + 2;
+			}
+			else {
+				swap(_heap[now * 2 + 1], _heap[now]);
+				now = now * 2 + 1;
+			}
+		}
+
+		return ret;
+	}
+};
+
+
+int main() {
+
+	heap<int> pq;
+	pq.push(3);
+	pq.push(4);
+	pq.push(1);
+	pq.push(11);
+	pq.push(6);
+
+	cout << pq.pop() << endl; // 11
+	cout << pq.pop() << endl; // 6
+	cout << pq.pop() << endl; // 4
+	cout << pq.pop() << endl; // 3
+	cout << pq.pop() << endl; // 1
+
+	cout << pq.pop() << endl; // 비어있음
+}

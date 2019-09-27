58f2b26f23d4a28c98df219c53b340bcebe0ecad
LEEJONGHWA <unknown9732@naver.com>
2019-09-27 15:07:50 +0900

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -5,25 +5,27 @@ class Node {
 public:
 	char c;
 	int use, finish; // 해당 노드 사용표시(초기화할 때 요긴하게 쓰임), 해당 알파벳이 단어의 끝인지 표시
-	Node *child[26]; // 알파벳 소문자는 26개
+	Node* child[26]; // 알파벳 소문자는 26개
 };
 
 Node heap[10000];
 int heapidx;
 
-void add(Node *node, char *str, int idx) {
+void add(Node* node, char* str, int idx) {
 	if (str[idx] == '\0') {
 		node->finish = 1;
 		return;
 	}
 	// 'a'는 아스키 코드값이 97 이므로..
-	node->child[str[idx] - 97] = &heap[heapidx++];
-	node->child[str[idx] - 97]->use = 1;
-	node->child[str[idx] - 97]->c = str[idx];
-	add(node->child[str[idx] - 97], str, idx + 1);	
+	if (node->child[str[idx] - 97] == nullptr || node->child[str[idx] - 97]->use == 0) {
+		node->child[str[idx] - 97] = &heap[heapidx++];
+		node->child[str[idx] - 97]->use = 1;
+		node->child[str[idx] - 97]->c = str[idx];
+	}
+	add(node->child[str[idx] - 97], str, idx + 1);
 }
 
-bool find(Node *node, char *str, int idx) {
+bool find(Node* node, char* str, int idx) {
 	while (str[idx] != '\0') { //입력받은 스트링의 끝까지 탐색
 		if (node->child[str[idx] - 97] == nullptr) return false;
 		node = node->child[str[idx] - 97];
@@ -34,7 +36,7 @@ bool find(Node *node, char *str, int idx) {
 
 
 int main() {
-	Node *head = &heap[heapidx++];
+	Node* head = &heap[heapidx++];
 	char str[10];
 	cin >> str;
 	add(head, str, 0);
